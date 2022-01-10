546b105faae9fac931932529dd3fcf1f0fe764fe
Grant Gillespie <gillegra@gmail.com>
2022-01-10 18:08:56 +0000

    Add core TS file with Day 1 solution

---
+++ public/js/solutions2021.js
@@ -0,0 +1,52 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const day01 = (input) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    //turn the input into an array of numbers, then count the number of times a number is greater than its predecessor, excluding the very first number
+    const numbers = input.split(",").map(Number);
+    let count = 0;
+    for (let i = 1; i < numbers.length; i++) {
+        let last = (_a = numbers[i - 1]) !== null && _a !== void 0 ? _a : numbers[i];
+        if (numbers[i] > last) {
+            count++;
+        }
+    }
+    return count;
+});
+const solutions = {
+    "day01": day01
+};
+const processFormSubmissions = (form) => __awaiter(void 0, void 0, void 0, function* () {
+    var _b, _c;
+    //get the input from the form and run it through the appropriate solution according to the left 5 characters from the form's id property
+    const day = form.id.substring(0, 5);
+    const input = (_c = (_b = form.querySelector(`#${day}Input`)) === null || _b === void 0 ? void 0 : _b.value) !== null && _c !== void 0 ? _c : "";
+    const solution = solutions[day];
+    const output = yield solution(input);
+    form.querySelector(`#${day}Output`).textContent += output;
+});
+//launch the app
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+const initApp = () => {
+    //add listeners to each form
+    const forms = document.querySelectorAll("form");
+    for (let i = 0; i < forms.length; i++) {
+        const form = forms[i];
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            processFormSubmissions(event.target);
+        });
+    }
+};
---
+++ solutions/solutions2021.ts
@@ -0,0 +1,48 @@
+const day01 = async (input: string): Promise<number> => {
+  //turn the input into an array of numbers, then count the number of times a number is greater than its predecessor, excluding the very first number
+  const numbers: number[] = input.split(",").map(Number);
+  let count: number = 0;
+  for (let i: number = 1; i < numbers.length; i++) {
+    let last: number = numbers[i - 1] ?? numbers[i];
+    if (numbers[i] > last) {
+      count++;
+    }
+  }
+  return count;
+};
+
+type SolutionsKey = keyof typeof solutions;
+const solutions = {
+  "day01": day01
+};
+
+const processFormSubmissions = async (form: HTMLFormElement) => {
+  //get the input from the form and run it through the appropriate solution according to the left 5 characters from the form's id property
+  // const day: string = form.id.substring(0, 5);
+  const day: SolutionsKey = form.id.substring(0, 5) as SolutionsKey;
+  const input: string = (form.querySelector(`#${day}Input`) as HTMLInputElement)?.value ?? "";
+  const solution: Function = solutions[day];
+  const output: string = await solution(input);
+  (form.querySelector(`#${day}Output`) as HTMLElement).textContent += output;
+};
+
+//launch the app
+document.addEventListener("readystatechange", (event: Event) => {
+  if (document.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  //add listeners to each form
+  const forms: NodeListOf<HTMLFormElement> = document.querySelectorAll("form");
+  for (let i: number = 0; i < forms.length; i++) {
+    const form: HTMLFormElement = forms[i];
+    form.addEventListener("submit", (event: SubmitEvent) => {
+      event.preventDefault();
+      processFormSubmissions(event.target as HTMLFormElement);
+    });
+  }
+};
+
+  
