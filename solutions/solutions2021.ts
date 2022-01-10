5eec473e44fd6e2a5631b8e9685a25e18ae5bf58
Grant Gillespie <gillegra@gmail.com>
2022-01-10 19:51:30 +0000

    Cleanup and rearrange daily functions to bottom
    By "Cleanup" I mean allow TS to do a little more type inference.

--- solutions/solutions2021.ts
+++ solutions/solutions2021.ts
@@ -1,24 +1,5 @@
-const day01 = async (input: string): Promise<number> => {
-  //turn the input into an array of numbers, then count the number of times a number is greater than its predecessor, excluding the very first number
-  const numbers: number[] = input.split("\n").map(Number);
-  let count: number = 0;
-  for (let i: number = 1; i < numbers.length; i++) {
-    let last: number = numbers[i - 1] ?? numbers[i];
-    if (numbers[i] > last) {
-      count++;
-    }
-  }
-  return count;
-};
-
-type SolutionsKey = keyof typeof solutions;
-const solutions = {
-  "day01": day01
-};
-
 const processFormSubmissions = async (form: HTMLFormElement) => {
   //get the input from the form and run it through the appropriate solution according to the left 5 characters from the form's id property
-  // const day: string = form.id.substring(0, 5);
   const day: SolutionsKey = form.id.substring(0, 5) as SolutionsKey;
   const input: string = (form.querySelector(`#${day}Input`) as HTMLInputElement)?.value ?? "";
   const solution: Function = solutions[day];
@@ -36,7 +17,7 @@ document.addEventListener("readystatechange", (event: Event) => {
 const initApp = () => {
   //prep each form with listeners and default values
   const forms: NodeListOf<HTMLFormElement> = document.querySelectorAll("form");
-  for (let i: number = 0; i < forms.length; i++) {
+  for (let i = 0; i < forms.length; i++) {
     const form: HTMLFormElement = forms[i];
 
     //submit button runs associated day's solution
@@ -52,4 +33,24 @@ const initApp = () => {
   }
 };
 
-  
+
+//SOLUTIONS
+
+//Day 1 part 1
+const day01 = async (input: string) => {
+  //turn the input into an array of numbers, then count the number of times a number is greater than its predecessor, excluding the very first number
+  const numbers = input.split("\n").map(Number);
+  let count: number = 0;
+  for (let i = 1; i < numbers.length; i++) {
+    let last = numbers[i - 1] ?? numbers[i];
+    if (numbers[i] > last) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const solutions = {
+  "day01": day01
+};
+type SolutionsKey = keyof typeof solutions;
